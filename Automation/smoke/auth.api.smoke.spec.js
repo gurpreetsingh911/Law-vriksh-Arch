da119e9c908dc48115cb9d228d992f3ffed43692
Gurpreet Singh <gpswazir@gmail.com>
2026-06-23 20:31:27 +0530

    added profile status

--- Automation/smoke/auth.api.smoke.spec.js
+++ Automation/smoke/auth.api.smoke.spec.js
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 //import { apiPost, validateSuccess, validateLoginBody, validateSecurity, validateFailure } from 'Law-vriksh-Arch\utils\api-helper.js';
 
 
-const EMAIL = 'gpswazir@gmail.com';
-const PASS = 'ZXcvbnm@123';
+const EMAIL = 'professional@lawvriksh.in';
+const PASS = 'Professional@123';
 const DEVICE_ID = '11BwnGUw7gupnSkGW5h7PkGdoU3ueGx6dDU';
-const ENDPOINT = 'https://testing.lawvriksh.in/api/auth/login/';
+const ENDPOINT = 'https://dev-api.lawvriksh.in/api/auth/login';
 const LOGOUT = 'https://dev-api.lawvriksh.in/api/auth/logout';
 
 let response;
@@ -45,36 +45,7 @@ let response;
 
 
 
-// test('LGN-02: Not login with invalid credentials', async ({ request }) => {
-//   response = await request.post(ENDPOINT, {
-
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json',
-//       'Referer': 'https://testing.lawvriksh.in/login', // Mimicking the browser
-//     },
-//     data: {
-//       request: {
-//         device_id: DEVICE_ID, // Use the ID from your logs
-//         email: EMAIL,
-//         password: PASS,
-//         remember_me: false
-//       }
-//     }
-//   });
-
-//   expect(response.status()).toBe(200);
-
-
-//   const data = await response.json();
-//  // console.log("responseBody:", data);
-
-
-// });
-
-
-test('LGN-03: Logout', async ({ request }) => {
-
+test('LGN-02: login with invalid credentials', async ({ request }) => {
   response = await request.post(ENDPOINT, {
 
     headers: {
@@ -94,39 +65,68 @@ test('LGN-03: Logout', async ({ request }) => {
 
   expect(response.status()).toBe(200);
 
+
   const data = await response.json();
-  const token = data.access_token;  // token_type: 'bearer',
-
-
-  const logoutResponse = await request.post(LOGOUT, {
-
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-      'Referer': 'https://dev-api.lawvriksh.in/api/auth/logout', // Mimicking the browser
-    },
-
-  });
-
-  console.log(`Logout Status Code: ${logoutResponse.status()}`);
-
-  // 3. Assertions to confirm the server accepted the logout request
-  expect(logoutResponse.status()).toBe(200);
-
-  // Safely check if the logout endpoint responds with JSON
-  const contentType = logoutResponse.headers()['content-type'];
-  if (contentType && contentType.includes('application/json')) {
-    const logoutData = await logoutResponse.json();
-    console.log("Logout Response Body:", logoutData);
-  } else {
-    console.log("Logout successful, raw text response:", await logoutResponse.text());
-  }
-
-
+  console.log("responseBody:", data);
 
 
 });
+
+
+// test('LGN-03: Logout', async ({ request }) => {
+
+//   response = await request.post(ENDPOINT, {
+
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//       'Referer': 'https://testing.lawvriksh.in/login', // Mimicking the browser
+//     },
+//     data: {
+//       request: {
+//         device_id: DEVICE_ID, // Use the ID from your logs
+//         email: EMAIL,
+//         password: PASS,
+//         remember_me: false
+//       }
+//     }
+//   });
+
+//   expect(response.status()).toBe(200);
+
+//   const data = await response.json();
+//   const token = data.access_token;  // token_type: 'bearer',
+
+
+//   const logoutResponse = await request.post(LOGOUT, {
+
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${token}`,
+//       'Referer': 'https://dev-api.lawvriksh.in/api/auth/logout', // Mimicking the browser
+//     },
+
+//   });
+
+//   console.log(`Logout Status Code: ${logoutResponse.status()}`);
+
+//   // 3. Assertions to confirm the server accepted the logout request
+//   expect(logoutResponse.status()).toBe(200);
+
+//   // Safely check if the logout endpoint responds with JSON
+//   const contentType = logoutResponse.headers()['content-type'];
+//   if (contentType && contentType.includes('application/json')) {
+//     const logoutData = await logoutResponse.json();
+//     console.log("Logout Response Body:", logoutData);
+//   } else {
+//     console.log("Logout successful, raw text response:", await logoutResponse.text());
+//   }
+
+
+
+
+// });
 
 
 
--- Automation/smoke/profile.api.smoke.spec.js
+++ Automation/smoke/profile.api.smoke.spec.js
@@ -10,8 +10,83 @@
 
 //https://dev-api.lawvriksh.in/dashboard/user/profile
 
-"profession": "Corporate Lawyer",
-            "email": "professional@lawvriksh.in",
-            "role": "professional",
-    "message": "User profile retrieved successfully",
-USR-01: Fetch logged-in user account settings
+// "profession": "Corporate Lawyer",
+//             "email": "professional@lawvriksh.in",
+//             "role": "professional",
+//     "message": "User profile retrieved successfully",
+// USR-01: Fetch logged-in user account settings
+
+
+import { test, expect } from '@playwright/test';
+//import { apiPost, validateSuccess, validateLoginBody, validateSecurity, validateFailure } from 'Law-vriksh-Arch\utils\api-helper.js';
+
+
+const EMAIL = 'professional@lawvriksh.in';
+const PASS = 'Professional@123';
+const DEVICE_ID = '11BwnGUw7gupnSkGW5h7PkGdoU3ueGx6dDU';
+const ENDPOINT = 'https://dev-api.lawvriksh.in/api/auth/login';
+const USERPROFILE = 'https://dev-api.lawvriksh.in/api/users/profile';
+
+let response;
+
+test('USR-01: Fetch logged-in user account settings', async ({ request }) => {
+  response = await request.post(ENDPOINT, {
+
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Referer': 'https://dev-api.lawvriksh.in/api/auth/login', // Mimicking the browser
+    },
+    data: {
+      request: {
+        device_id: DEVICE_ID, // Use the ID from your logs
+        email: EMAIL,
+        password: PASS,
+        remember_me: false
+      }
+    }
+  });
+
+  expect(response.status()).toBe(200);
+
+  const data = await response.json();
+   
+  const token = data.data.access_token;  // token_type: 'bearer',
+
+  //const two = data.token_type;  // token_type: 'bearer',
+
+    
+  //console.log("message:", token);
+
+
+
+  const Userresponse = await request.get(USERPROFILE, {
+
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+      'Referer': USERPROFILE, // Mimicking the browser
+    },
+
+  });
+  
+
+    const second_data = await Userresponse.json();
+
+    const message = second_data.message;  // message: 'User profile retrieved successfully',
+    const profession = second_data.data.profile.profession;  // profession: 'Corporate Lawyer',
+  const email = second_data.data.profile.email;  // email: '
+
+    expect(email).toMatch(EMAIL);
+
+    console.log("responseBody:", message, profession, email);
+  
+
+
+
+
+
+
+
+});
